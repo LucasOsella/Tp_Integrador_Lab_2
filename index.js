6781b841209574b5f82278083e8aac156c8576ce
LucasOsella <lucasosella01@gmail.com>
2024-10-31 20:22:29 -0300

    Insertado crear, listar, layout

--- index.js
+++ index.js
@@ -16,8 +16,9 @@ const conn = mysql.createConnection({
 app.set('view engine', 'pug');
 app.set('views','views');
 app.use(express.static('public'));
+app.use(express.urlencoded());
 
-app.get('/medicos', (req, res) => {
+app.get('/listarMedicos', (req, res) => {
     conn.query('SELECT * FROM medicos', (err, rows) => {
         if (err) {
             console.log(err,"No se puedo consutar la base de datos de los medicos");   
@@ -27,6 +28,38 @@ app.get('/medicos', (req, res) => {
 
     
 });
+
+app.get('/medicos/create', (req, res) => {
+    res.render('medicos/crear');
+});
+
+app.post('/medicos', (req, res) => {
+    const nombre = req.body.nombre;
+    const apellido = req.body.apellido;
+    const dni = req.body.dni;
+    const mail = req.body.mail;
+    const telefono = req.body.telefono;
+    let estado = req.body.estado;
+    if (estado === 'on') {
+        estado = 1;
+    } else {
+        estado = 0;
+    }
+    const fecha_alta = req.body.fecha;
+
+    const sql = 'INSERT INTO `medicos`(`nombre`, `apellido`, `dni`, `mail`, `telefono`, `estado`, `fecha_alta`) VALUES (?,?,?,?,?,?,?)';
+    const values = [nombre, apellido, dni, mail, telefono, estado, fecha_alta];
+
+    conn.query(sql, values, (err, rows) => {
+        if (err) {
+            console.log(err, "No se pudo registrar el médico en la base de datos");
+            res.status(500).send("Error al registrar el médico");
+        } else {
+            res.redirect('/listarMedicos');
+        }
+    });
+});
+
 
 app.listen(3000, () => {
     console.log('Server listening on port 3000');
